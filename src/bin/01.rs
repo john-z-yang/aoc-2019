7f5c5309b371cd7855ade80a845a24f19b637f8c
John Yang <johnyang@protonmail.com>
2024-04-13 12:52:06 -0700

    Solve day 1 part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,17 +1,41 @@
 advent_of_code::solution!(1);
 
-pub fn part_one(input: &str) -> Option<u32> {
+pub fn part_one(input: &str) -> Option<i64> {
     Some(
         input
             .split('\n')
-            .filter_map(|line| line.parse::<u32>().ok())
-            .map(|mass| mass / 3 - 2)
+            .filter_map(|line| line.parse::<i64>().ok())
+            .map(get_fuel_cost)
             .sum(),
     )
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<i64> {
+    Some(
+        input
+            .split('\n')
+            .filter_map(|line| line.parse::<i64>().ok())
+            .map(get_total_fuel_cost)
+            .sum(),
+    )
+}
+
+fn get_total_fuel_cost(mut mass: i64) -> i64 {
+    let mut res = 0i64;
+    while get_fuel_cost(mass) > 0 {
+        res += get_fuel_cost(mass);
+        mass = get_fuel_cost(mass);
+    }
+    res
+}
+
+fn get_fuel_cost(mass: i64) -> i64 {
+    let fuel = mass / 3 - 2;
+    if fuel > 0 {
+        fuel
+    } else {
+        0
+    }
 }
 
 #[cfg(test)]
@@ -21,12 +45,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(34239));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2 + 966 + 50346));
     }
 }
